8a02d6443badb62359e222bb56398d31dee99856
odintk <1625181109@qq.com>
2022-06-02 09:30:35 +0800

    新建

--- src/main/java/com/atguigu/gittest.java
+++ src/main/java/com/atguigu/gittest.java
@@ -8,5 +8,7 @@ package com.atguigu;
 public class gittest {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
     }
 }
