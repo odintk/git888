d915ab9e35434496e861c5ca10901ed592486247
odintk <1625181109@qq.com>
2022-06-02 09:24:08 +0800

    新建

---
+++ src/main/java/com/atguigu/gittest.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * Date : {2022/6/2}
+ * Author : mzp
+ * Description :
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
